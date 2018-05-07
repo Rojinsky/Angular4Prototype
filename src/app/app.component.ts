4eac1c773089eccfd04170d455552526e752ee0b
analistadllo1 <analistadllo1@gmail.com>
2018-05-06 22:15:08 -0500

    haciendo efectos de entrada con animated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { InformacionService } from "./services/informacion.service";
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: ['./styles.css']
 })
 export class AppComponent {
   constructor( public _is:InformacionService ){
