592a49f9e068acba687001a8fefce914bd01f656
analistadllo1 <analistadllo1@gmail.com>
2018-05-14 12:44:23 -0500

    se conecto con el firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,29 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargadaSobre: boolean = false;
+equipo:any[]=[];
 
   constructor( public http:Http ) {
 
-    this.http.get("assets/data/info.pagina.json")
-    .subscribe( data => {
-          console.log(data.json());
-          this.cargada = true;
-          this.info = data.json();
-    })
+    this.carga_infor();
+    this.carga_sobre_nosotros();
    }
 
+   public carga_infor(){
+     this.http.get("assets/data/info.pagina.json")
+       .subscribe(data => {
+         // console.log(data.json());
+         this.cargada = true;
+         this.info = data.json();
+       })
+   }
+  public carga_sobre_nosotros(){
+    this.http.get("https://elprimero-92dee.firebaseio.com/equipo.json")
+      .subscribe(data => {
+        console.log(data.json());
+        this.cargadaSobre = true;
+        this.equipo = data.json();
+      }) 
+  }
 }
